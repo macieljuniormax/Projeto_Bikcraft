bcee082dd463162764d599972e11d5e6ba6209b1
Maciel Júnior <macielfcjunior@icloud.com>
2021-06-08 17:22:19 -0300

    Adcionando Slides na página Portfólio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    //auto: true, // se o slide deve passar automaticamente
+    // nav: true, // se deve ou não mostrar a navegação
+    // pauseOnHover: true, // pausa a transição automática
+  });
+
+  new SimpleSlide({
+    slide: 'portfolio', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    //auto: true, // se o slide deve passar automaticamente
+    nav: true, // se deve ou não mostrar a navegação
+    // pauseOnHover: true, // pausa a transição automática
+  });
+}
