a7fb2c94d8bb4753279807ccbb9c062a940c46f7
Maciel Júnior <macielfcjunior@icloud.com>
2021-06-08 20:52:42 -0300

    Implementação de animações na página

--- js/script.js
+++ js/script.js
@@ -2,17 +2,12 @@ if (window.SimpleSlide) {
   new SimpleSlide({
     slide: 'quote', // nome do atributo data-slide="principal"
     time: 5000, // tempo de transição dos slides
-    //auto: true, // se o slide deve passar automaticamente
-    // nav: true, // se deve ou não mostrar a navegação
-    // pauseOnHover: true, // pausa a transição automática
   });
 
   new SimpleSlide({
-    slide: 'portfolio', // nome do atributo data-slide="principal"
-    time: 5000, // tempo de transição dos slides
-    //auto: true, // se o slide deve passar automaticamente
-    nav: true, // se deve ou não mostrar a navegação
-    // pauseOnHover: true, // pausa a transição automática
+    slide: 'portfolio',
+    time: 5000,
+    nav: true,
   });
 }
 
